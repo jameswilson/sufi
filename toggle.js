f24345f82741af18ad7a02281c44287a3f318062
James Wilson <jrwilson3@gmail.com>
2024-09-30 00:26:54 -0500

    Initial commit

---
+++ toggle.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const id = ".toggle #light";
+  const checkbox = document.querySelector(id);
+
+  // Check if the localStorage variable exists and update the checkbox state.
+  checkbox.checked = localStorage.getItem(id);
+
+  // Monitor for changes to the checkbox state.
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      // Set the localStorage item when checkbox is checked.
+      localStorage.setItem(id, true);
+    } else {
+      // Remove the localStorage item when checkbox is unchecked.
+      localStorage.removeItem(id);
+    }
+  });
+});
