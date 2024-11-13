4a5e9c5cf72b9efe048d080916474b04a59d4492
James Wilson <jrwilson3@gmail.com>
2024-11-13 02:16:23 -0500

    Add page for pranayama

--- toggle.js
+++ toggle.js
@@ -1,13 +1,33 @@
+
+// Toggle light mode.
+const id = "#light-mode";
+
 document.addEventListener("DOMContentLoaded", function () {
 
-  const id = "#light-mode";
   const checkbox = document.querySelector(id);
 
   // Check if the localStorage variable exists and update the checkbox state.
-  checkbox.checked = localStorage.getItem(id);
+  if (checkbox) {
+    checkbox.checked = localStorage.getItem(id);
 
-  // Monitor for changes to the checkbox state.
-  checkbox.addEventListener("change", function () {
-    localStorage.setItem(id, checkbox.checked);
-  });
+    // Monitor for changes to the checkbox state.
+    checkbox.addEventListener("change", function () {
+      localStorage.setItem(id, checkbox.checked);
+    });
+  }
 });
+
+window.addEventListener("storage", (event) => {
+  if (event.key === id) {
+    const checkbox = document.querySelector(id);
+    if (checkbox) {
+      checkbox.checked = event.newValue === "true";
+    }
+  }
+});
+
+if (window.self !== window.top) {
+  document.documentElement.classList.add("iframe");
+} else {
+  document.documentElement.classList.remove("iframe");
+}
